1e607cfde2d452845a46d29232b854a404009973
Kristian Hovde Liland <kristian.liland@nmbu.no>
2020-06-12 14:06:25 +0200

    Added plottable output

--- CNN_script.py
+++ CNN_script.py
@@ -1,7 +1,9 @@
-# Don't touch!
+# Don't touch! ------->
 import httpimport
 with httpimport.remote_repo(['Simple_CNN'], 'https://raw.githubusercontent.com/khliland/OrangeCNN/master'):
     from Simple_CNN import *
+# <------- Don't touch!
+
 
 ######################
 ## Apply simple CNN ##
@@ -12,6 +14,9 @@ epochs        = 100
 batch_size    = 64
 learning_rate = 0.0001
 
-(X_train, Y_train, X_test, Y_test) = dataPrep(in_data)
-history = CNN(classify, batch_size=batch_size, epochs=epochs, lr=learning_rate)
 
+# Don't touch! ------->
+(X_train, Y_train, X_test, Y_test) = dataPrep(in_data)
+history = CNN(classify, X_train=X_train, Y_train=Y_train, X_test=X_test, Y_test=Y_test, batch_size=batch_size, epochs=epochs, lr=learning_rate)
+out_data = Table.from_numpy(None,np.hstack([np.array(history.history['accuracy'])[:,np.newaxis], np.arange(1,epochs + 1,1)[:,np.newaxis]]))
+# <------- Don't touch!
